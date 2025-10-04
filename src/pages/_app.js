2a15556779180b3b57fb8674205277bf555b2a83
miknivin <116654570+miknivin@users.noreply.github.com>
2025-10-05 00:33:28 +0530

    pixel code added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,11 +13,42 @@ import { ToastContainer } from "react-toastify";
 import { store } from "@/store/store";
 import UserProfile from "@/components/auth/UserProfile";
 import { Analytics } from "@vercel/analytics/next";
+import Head from "next/head";
+import Script from "next/script";
+
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <Head>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1497561568234262&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </Head>
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        src="https://connect.facebook.net/en_US/fbevents.js"
+      />
+      <Script id="facebook-pixel-init" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[]}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1497561568234262');
+          fbq('track', 'PageView');
+        `}
+      </Script>
       <Component {...pageProps} />
       <UserProfile />
       <ToastContainer
