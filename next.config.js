da0033737b1f689e48d284fccf15753a66b64bb4
miknivin <116654570+miknivin@users.noreply.github.com>
2025-11-10 13:25:48 +0530

    change in next config

--- next.config.js
+++ next.config.js
@@ -2,21 +2,37 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "kids-bags.s3.eu-north-1.amazonaws.com",
-      "d229x2i5qj11ya.cloudfront.net",
-      "scontent.cdninstagram.com",
-      "instagram.com",
-      "ik.imagekit.io",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "kids-bags.s3.eu-north-1.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "d229x2i5qj11ya.cloudfront.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "scontent.cdninstagram.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        pathname: "/**",
+      },
     ],
   },
+
   async redirects() {
     if (process.env.MAINTENANCE_MODE === "true") {
       return [
         {
           source: "/((?!maintenance|_next|_vercel|.*\\..*).*)",
           destination: "/maintenance",
-          permanent: false, // 302 redirect
+          permanent: false,
         },
       ];
     }
